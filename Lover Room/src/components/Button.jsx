5ae3388329fc223c4eab6d2dbc1e42fe3096e113
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-02-09 22:36:46 +0530

    created a button

---
+++ Lover Room/src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Button() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="bg-gray-200 p-8 rounded-lg">
+        <button className="text-2xl font-bold">Click me</button>
+      </div>
+    </div>
+  )
+}
